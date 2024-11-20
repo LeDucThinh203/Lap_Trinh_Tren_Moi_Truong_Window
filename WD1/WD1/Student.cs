d97000a6b9cae2b239b80de0bee56424ea2753a8
LeDucThinh203 <leducthinh203@gmail.com>
2024-11-20 22:57:24 +0700

    Buoi2

---
+++ WD1/WD1/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WD1
+{
+    internal class Student
+    {
+        public int Id;
+        public string Name;
+        public int Age;
+        public int ID {  get; set; }
+        public string NAME { get; set; }
+        public int AGE { get; set; }
+    }
+}
